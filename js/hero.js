e1f95dc8df00d9642988adb0b959bb1a08828cb5
Hannah Ahn <ahnlook90@gmail.com>
2023-07-07 20:58:24 +0900

    fix: corrected typo in variable name

--- js/hero.js
+++ js/hero.js
@@ -5,7 +5,7 @@ class Hero {
   #imageSize
   #currentImage
   #viewportPosition
-  
+
   constructor() {
     this.#directionBtn = $('.carousel-directionArrows')
     this.#viewport = this.#directionBtn.previousElementSibling.firstElementChild
@@ -18,13 +18,13 @@ class Hero {
     this.addEvents()
     this.autoSlide()
   }
-  
+
   addEvents() {
     this.#directionBtn.addEventListener('click', ({ target }) => {
-      const [leftButton, rightButton] = 
-        ['.carousel-left', '.carousel-right'].map(className => 
-          target.closest(className)
-        )
+      const [leftButton, rightButton] = [
+        '.carousel-left',
+        '.carousel-right'
+      ].map(className => target.closest(className))
 
       if (leftButton) this.slidePrev()
       if (rightButton) this.slideNext()
@@ -37,7 +37,7 @@ class Hero {
         this.#viewportPosition = -9000
         this.#currentImage = 6
       }
-      
+
       if (this.#currentImage >= 7) {
         this.#viewport.style.transition = 'none'
         this.#viewport.style.transform = 'translateX(-1500px)'
@@ -53,7 +53,7 @@ class Hero {
       this.slideNext()
     }, DELAY_TIME)
   }
-  
+
   slidePrev() {
     this.#currentImage -= 1
     this.#viewportPosition += this.#imageSize
@@ -69,5 +69,5 @@ class Hero {
   }
 }
 
-const hreo = new Hero()
-hreo.init()
+const hero = new Hero()
+hero.init()
